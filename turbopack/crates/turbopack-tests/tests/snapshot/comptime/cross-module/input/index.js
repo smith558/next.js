0a8a8234e9862b07db6a12616983c1b103c412b6
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-02-21 12:53:48 +0100

    Revert "WIP ConstantsModuleObject"
    This reverts commit a260c2398c5ce5896626ac51402b39ffc81673c4.

--- turbopack/crates/turbopack-tests/tests/snapshot/comptime/cross-module/input/index.js
+++ turbopack/crates/turbopack-tests/tests/snapshot/comptime/cross-module/input/index.js
@@ -1,12 +1,6 @@
-import { SOME_VALUE, MISSING } from './other.constants'
+import { SOME_VALUE } from './other.constants'
 
 if (SOME_VALUE === 'x') {
-  console.log('x')
-} else {
-  console.log('dead')
-}
-
-if (MISSING === 'x') {
   console.log('x')
 } else {
   console.log('dead')
